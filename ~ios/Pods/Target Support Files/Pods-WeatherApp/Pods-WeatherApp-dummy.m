25f82d6b17d4e21e2662b66102638ebdcaff2681
Julian Moyse <julian.moyse@gmail.com>
2018-05-04 11:33:20 -0400

    Removing junk ios folder

--- ~ios/Pods/Target Support Files/Pods-WeatherApp/Pods-WeatherApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WeatherApp : NSObject
-@end
-@implementation PodsDummy_Pods_WeatherApp
-@end
--- ~ios/Pods/Target Support Files/Pods-WeatherAppTests/Pods-WeatherAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WeatherAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_WeatherAppTests
-@end
